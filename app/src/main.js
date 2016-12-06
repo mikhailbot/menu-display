db9320f4881652c3e0396f4c393a8f09e4fd89ba
mikhaildelport <mikhail@delport.ca>
2016-12-06 15:03:33 -0500

    Add English localization to Element library

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Electron from 'vue-electron'
 import Router from 'vue-router'
 import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-default/index.css'
 import './scheduler'
 
@@ -10,7 +11,7 @@ import routes from './routes'
 
 Vue.use(Electron)
 Vue.use(Router)
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
 Vue.config.debug = true
 
 const router = new Router({
